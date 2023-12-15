f8bb75a297dc1f004b2830eea4366fe1738495e8
Luanserph <darkserphps4@gmail.com>
2023-12-15 13:57:21 -0300

    formatação do codigo e otimização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 /**
-* Template Name: Append
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/append-bootstrap-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ * Template Name: Append
+ * Updated: Sep 18 2023 with Bootstrap v5.3.2
+ * Template URL: https://bootstrapmade.com/append-bootstrap-website-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
@@ -14,16 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
   let scrollTop = document.querySelector('.scroll-top');
 
   function toggleScrollTop() {
-    if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-    }
+      if (scrollTop) {
+          window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+      }
   }
   scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+      e.preventDefault();
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
   });
 
   window.addEventListener('load', toggleScrollTop);
@@ -34,9 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   const preloader = document.querySelector('#preloader');
   if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
+      window.addEventListener('load', () => {
+          preloader.remove();
+      });
   }
 
   /**
@@ -46,8 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const selectHeader = document.querySelector('#header');
 
   function toggleScrolled() {
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+      if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+      window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
 
   document.addEventListener('scroll', toggleScrolled);
@@ -58,21 +58,21 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   let lastScrollTop = 0;
   window.addEventListener('scroll', function() {
-    if (!selectHeader.classList.contains('scroll-up-sticky')) return;
+      if (!selectHeader.classList.contains('scroll-up-sticky')) return;
 
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop && scrollTop > selectHeader.offsetHeight) {
-      selectHeader.style.setProperty('position', 'sticky', 'important');
-      selectHeader.style.top = `-${header.offsetHeight + 50}px`;
-    } else if (scrollTop > selectHeader.offsetHeight) {
-      selectHeader.style.setProperty('position', 'sticky', 'important');
-      selectHeader.style.top = "0";
-    } else {
-      selectHeader.style.removeProperty('top');
-      selectHeader.style.removeProperty('position');
-    }
-    lastScrollTop = scrollTop;
+      if (scrollTop > lastScrollTop && scrollTop > selectHeader.offsetHeight) {
+          selectHeader.style.setProperty('position', 'sticky', 'important');
+          selectHeader.style.top = `-${header.offsetHeight + 50}px`;
+      } else if (scrollTop > selectHeader.offsetHeight) {
+          selectHeader.style.setProperty('position', 'sticky', 'important');
+          selectHeader.style.top = "0";
+      } else {
+          selectHeader.style.removeProperty('top');
+          selectHeader.style.removeProperty('position');
+      }
+      lastScrollTop = scrollTop;
   });
 
   /**
@@ -81,9 +81,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
   function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+      mobileNavToggleBtn.classList.toggle('bi-list');
+      mobileNavToggleBtn.classList.toggle('bi-x');
   }
   mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
 
@@ -91,11 +91,11 @@ document.addEventListener('DOMContentLoaded', () => {
    * Hide mobile nav on same-page/hash links
    */
   document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
-      }
-    });
+      navmenu.addEventListener('click', () => {
+          if (document.querySelector('.mobile-nav-active')) {
+              mobileNavToogle();
+          }
+      });
 
   });
 
@@ -103,39 +103,39 @@ document.addEventListener('DOMContentLoaded', () => {
    * Toggle mobile nav dropdowns
    */
   document.querySelectorAll('.navmenu .has-dropdown i').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      if (document.querySelector('.mobile-nav-active')) {
-        e.preventDefault();
-        this.parentNode.classList.toggle('active');
-        this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-        e.stopImmediatePropagation();
-      }
-    });
+      navmenu.addEventListener('click', function(e) {
+          if (document.querySelector('.mobile-nav-active')) {
+              e.preventDefault();
+              this.parentNode.classList.toggle('active');
+              this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+              e.stopImmediatePropagation();
+          }
+      });
   });
 
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
   window.addEventListener('load', function(e) {
-    if (window.location.hash) {
-      if (document.querySelector(window.location.hash)) {
-        setTimeout(() => {
-          let section = document.querySelector(window.location.hash);
-          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
-          window.scrollTo({
-            top: section.offsetTop - parseInt(scrollMarginTop),
-            behavior: 'smooth'
-          });
-        }, 100);
+      if (window.location.hash) {
+          if (document.querySelector(window.location.hash)) {
+              setTimeout(() => {
+                  let section = document.querySelector(window.location.hash);
+                  let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+                  window.scrollTo({
+                      top: section.offsetTop - parseInt(scrollMarginTop),
+                      behavior: 'smooth'
+                  });
+              }, 100);
+          }
       }
-    }
   });
 
   /**
    * Initiate glightbox
    */
   const glightbox = GLightbox({
-    selector: '.glightbox'
+      selector: '.glightbox'
   });
 
   /**
@@ -147,32 +147,32 @@ document.addEventListener('DOMContentLoaded', () => {
    * Init isotope layout and filters
    */
   function initIsotopeLayout() {
-    document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
-      let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-      let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-      let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+      document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+          let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+          let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+          let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
 
-      let initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-        itemSelector: '.isotope-item',
-        layoutMode: layout,
-        filter: filter,
-        sortBy: sort
-      });
-
-      isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-        filters.addEventListener('click', function() {
-          isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-          this.classList.add('filter-active');
-          initIsotope.arrange({
-            filter: this.getAttribute('data-filter')
+          let initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+              itemSelector: '.isotope-item',
+              layoutMode: layout,
+              filter: filter,
+              sortBy: sort
           });
-          if (typeof aosInit === 'function') {
-            aosInit();
-          }
-        }, false);
-      });
 
-    });
+          isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+              filters.addEventListener('click', function() {
+                  isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+                  this.classList.add('filter-active');
+                  initIsotope.arrange({
+                      filter: this.getAttribute('data-filter')
+                  });
+                  if (typeof aosInit === 'function') {
+                      aosInit();
+                  }
+              }, false);
+          });
+
+      });
   }
   window.addEventListener('load', initIsotopeLayout);
 
@@ -180,19 +180,19 @@ document.addEventListener('DOMContentLoaded', () => {
    * Frequently Asked Questions Toggle
    */
   document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-    faqItem.addEventListener('click', () => {
-      faqItem.parentNode.classList.toggle('faq-active');
-    });
+      faqItem.addEventListener('click', () => {
+          faqItem.parentNode.classList.toggle('faq-active');
+      });
   });
 
   /**
    * Init swiper sliders
    */
   function initSwiper() {
-    document.querySelectorAll('.swiper').forEach(function(swiper) {
-      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
-      new Swiper(swiper, config);
-    });
+      document.querySelectorAll('.swiper').forEach(function(swiper) {
+          let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
+          new Swiper(swiper, config);
+      });
   }
   window.addEventListener('load', initSwiper);
 
@@ -200,35 +200,35 @@ document.addEventListener('DOMContentLoaded', () => {
    * Animation on scroll function and init
    */
   function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
+      AOS.init({
+          duration: 600,
+          easing: 'ease-in-out',
+          once: true,
+          mirror: false
+      });
   }
   window.addEventListener('load', aosInit);
-  $('.php-email-form').submit(function(e){
-    e.preventDefault();
-    $.ajax({
-        type: 'POST',
-        url: 'forms/contact.php', // Seu arquivo PHP para enviar o email
-        data: $('.php-email-form').serialize(),
-        dataType: 'json', // Indica que o retorno é um JSON
-        success: function(response){
-            if(response.success){
-              alert('Email enviado com sucesso!');
-            } else {
-                // Se houver um erro no envio do email
-                alert(response.message);
-            }
-        },
-        error: function(){
-            // Se houver um erro na requisição AJAX
-            alert('Erro na requisição AJAX.');
-        }
-    });
-});
+  $('.php-email-form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: 'POST',
+          url: 'forms/contact.php', // Seu arquivo PHP para enviar o email
+          data: $('.php-email-form').serialize(),
+          dataType: 'json', // Indica que o retorno é um JSON
+          success: function(response) {
+              if (response.success) {
+                  alert('Email enviado com sucesso!');
+              } else {
+                  // Se houver um erro no envio do email
+                  alert(response.message);
+              }
+          },
+          error: function() {
+              // Se houver um erro na requisição AJAX
+              alert('Erro na requisição AJAX.');
+          }
+      });
+  });
 
 
 });
